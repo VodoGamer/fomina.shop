fd7d5396bbd76eee574b028bb795c32b3a214216
VodoGamer <vodogamer@yandex.ru>
2023-07-20 11:02:08 +0300

    ui(assets): import background image on index as tsx url

--- ui/src/pages/Index.tsx
+++ ui/src/pages/Index.tsx
@@ -2,12 +2,14 @@ import { type Component } from 'solid-js';
 
 import Header from '../Header';
 
+import hello_image from "../../static/images/hello-image.webp";
+
 const Index: Component = () => {
 	return (
 		<>
 			<Header />
 			<main class="main">
-				<div class="first-look" style="background-image: url(/static/images/hello-image.webp);">
+				<div class="first-look" style={`background-image: url(${hello_image});`}>
 					<h1 class="first-look__header">Fomina style</h1>
 				</div>
 			</main>
