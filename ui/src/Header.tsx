c8a9113fe89ce57a566bf569ed5115bd211c7d6a
VodoGamer <vodogamer@yandex.ru>
2023-07-20 10:58:34 +0300

    ui: importing Assets as tsx URL

--- ui/src/Header.tsx
+++ ui/src/Header.tsx
@@ -1,12 +1,15 @@
 import { Component } from "solid-js";
+
 import BurgerMenu from "./BurgerMenu";
+
+import logo from "../static/icons/fomina-logo.svg";
 
 const Header: Component = () => {
 	return (
 		<>
 			<header class="header">
 				<BurgerMenu />
-				<a href="/" aria-label="Перейти на главную страницу"><img class="header__logo" width="284" src="/static/icons/fomina-logo.svg" alt="" /></a>
+				<a href="/" aria-label="Перейти на главную страницу"><img class="header__logo" width="284" src={logo} alt="" /></a>
 			</header>
 		</>
 	)
