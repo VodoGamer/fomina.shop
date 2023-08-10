996a6b30576ac9ba4b0e3394e44ffb3b0cde61b3
VodoGamer <vodogamer@yandex.ru>
2023-08-10 20:36:42 +0300

    ♻️ ui: small refactor cart

--- ui/src/pages/Cart.tsx
+++ ui/src/pages/Cart.tsx
@@ -1,37 +1,51 @@
-import { type Component, createResource, Show } from "solid-js";
+import { type Component, createResource, Show, For } from "solid-js";
 import axios from "axios";
 
 import Header from "../Header";
 import CartProduct from "../components/CartProduct";
+import { Product } from "../types/product";
 
 
-const getProducts = async (product_ids: string[]) =>
-	(await axios.get(`${import.meta.env.VITE_BASE_API_URL}/product/${product_ids}/`)).data;
+const getProducts = async (productIds: string[]): Promise<Product[]> => {
+	const returnList: Product[] = [];
+	for (const productId of productIds) {
+		returnList.push((await axios.get(`${import.meta.env.VITE_BASE_API_URL}/product/${productId}/`)).data);
+	};
+	return returnList;
+}
+
+const calculateOverallSum = (products: Product[]): number => {
+	let sum: number = 0;
+	for (const product of products) { sum += product.price; }
+	return sum;
+}
 
 const Cart: Component = () => {
 	const product_ids = localStorage.getItem("cartProducts")?.split(",")
-	const [product] = createResource(product_ids, getProducts);
+	const [products] = createResource(product_ids, getProducts);
+	const [overallSum] = createResource(products, calculateOverallSum)
 
 	return (
 		<>
 			<Header />
 			<main>
-				<h1 class="cart-header">Ваша корзина</h1>
-				<div class="cart">
-					<div class="cart-products">
-						<Show when={product()} fallback="Товары загружаются...">
-							<CartProduct product={product()} />
-						</Show>
+				<Show when={products()} fallback={<h1 class="cart-header">Ваша корзина пуста :(</h1>}>
+					<h1 class="cart-header">Ваша корзина</h1>
+					<div class="cart">
+						<div class="cart-products">
+							<For each={products()}>{(product) =>
+								<CartProduct product={product} />
+							}</For>
+						</div>
+						<div class="cart-information">
+							<h3 class="cart-information__text">Предметов в корзине: {product_ids?.length}</h3>
+							<h3 class="cart-information__text">Итого к оплате: <Show when={overallSum}>{overallSum()}</Show>₽</h3>
+							<a class="order-button" href="/">
+								<h4 class="order-button__text">Перейти к оформлению</h4>
+							</a>
+						</div>
 					</div>
-					<div class="cart-information">
-						<h3 class="cart-information__text">Предметов в корзине: {product_ids?.length}</h3>
-						<h3 class="cart-information__text">Итого к оплате: 5500₽</h3>
-						{/* TODO: overall sum */}
-						<a class="order-button" href="/">
-							<h4 class="order-button__text">Перейти к оформлению</h4>
-						</a>
-					</div>
-				</div>
+				</Show>
 			</main >
 		</>
 	);
