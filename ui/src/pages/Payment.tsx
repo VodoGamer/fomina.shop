f886e2ace67f0143f9b6d170d6bb7fc334f0e5b7
VodoGamer <vodogamer@yandex.ru>
2023-08-18 12:42:05 +0300

    💄 ui: make `payment` page reactive

--- ui/src/pages/Payment.tsx
+++ ui/src/pages/Payment.tsx
@@ -1,33 +1,52 @@
-import { Component } from "solid-js";
+import { Component, Match, Switch, createResource } from "solid-js";
 import { useParams } from "@solidjs/router";
 
 import Header from "../components/Header";
 
 import success from "../../static/icons/success.svg"
 import processing from "../../static/icons/processing.svg"
+import { Order } from "../types/order";
+import axios from "axios";
 
 type PaymentParams = {
 	id: string,
 }
 
+async function getPayment(paymentId: string): Promise<Order> {
+	return (
+		await axios.get(`${import.meta.env.VITE_BASE_API_URL}/payment`, {
+			"params":
+				{ "payment_id": paymentId }
+		})
+	).data;
+}
+
 const Payment: Component = () => {
 	const params = (useParams() as PaymentParams);
+	const [payment, { refetch }] = createResource(params.id, getPayment);
+	setInterval(refetch, 5000);
 
 	return (
 		<>
 			<Header />
 			<h1>Успешная покупка!</h1>
-			<div class="payment-description">
-				<img class="payment-description__icon" src={processing} alt="" width="48px" />
-				<p class="payment-description__text">Проверяем оплату...</p>
-			</div>
-			<div class="payment-description">
-				<img class="payment-description__icon" src={success} alt="" width="48px" />
-				<p class="payment-description__text">Вы успешно оплатили заказ №<b>{params.id}</b>. Мы в скором времени напишем вам на почту. Спасибо что выбрали нас❤️‍🔥.</p>
-			</div>
-			<a class="order-button" href="/">
-				<h4 class="order-button__text">Перейти на главную</h4>
-			</a>
+			<Switch>
+				<Match when={payment()?.status != "succeeded"}>
+					<div class="payment-description">
+						<img class="payment-description__icon" src={processing} alt="" width="48px" />
+						<p class="payment-description__text">Проверяем оплату... Это будет недолго</p>
+					</div>
+				</Match>
+				<Match when={payment()?.status == "succeeded"}>
+					<div class="payment-description">
+						<img class="payment-description__icon" src={success} alt="" width="48px" />
+						<p class="payment-description__text">Вы успешно оплатили заказ №<b>{params.id}</b>. Мы в скором времени напишем вам на почту. Спасибо что выбрали нас❤️‍🔥.</p>
+					</div>
+					<a class="order-button" href="/">
+						<h4 class="order-button__text">Перейти на главную</h4>
+					</a>
+				</Match>
+			</Switch>
 		</>
 	);
 };
