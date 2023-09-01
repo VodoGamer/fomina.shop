0de6df8a70212e879b4062a2209da42e47965398
VodoGamer <vodogamer@yandex.ru>
2023-09-01 18:41:50 +0300

    💄 ui: add cart icon to header

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -3,6 +3,7 @@ import { Component } from "solid-js";
 import BurgerMenu from "./BurgerMenu";
 
 import logo from "../../static/icons/fomina-logo.svg";
+import cart from "../../static/icons/shopping_cart.svg";
 
 const Header: Component = () => {
 	return (
@@ -11,6 +12,9 @@ const Header: Component = () => {
 				<BurgerMenu />
 				<a href="/" aria-label="Перейти на главную страницу">
 					<img class="header__logo" width="284" src={logo} alt="" />
+				</a>
+				<a href="/cart" aria-label="Перейти на страницу корзины">
+					<img src={cart} alt="" />
 				</a>
 			</header>
 		</>
