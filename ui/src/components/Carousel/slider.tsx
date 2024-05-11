82a02b99d99bf5593c29863afee3590938bcad76
Andrey Smirnov <54740235+VodoGamer@users.noreply.github.com>
2024-05-11 18:43:57 +0300

    ✨ feat: add carousel for product images (#2)
    * ✨ feat: add carousel for product images
    
    * ✨ feat: carousel start from user clicked picture

---
+++ ui/src/components/Carousel/slider.tsx
@@ -0,0 +1,25 @@
+import { For } from "solid-js";
+import type Image from "../../interfaces/image";
+import { getImageUrl } from "../../utils/images";
+
+import { Slider, SliderProvider } from "solid-slider";
+import styles from "./carousel.module.sass";
+import "./slider.css";
+
+export default function ImagesSlider(props: { images: Image[] }) {
+	return (
+		<SliderProvider>
+			<Slider options={{ loop: true }}>
+				<For each={props.images}>
+					{(image) => (
+						<img
+							class={styles.image}
+							src={getImageUrl(image.url)}
+							alt={image.description}
+						/>
+					)}
+				</For>
+			</Slider>
+		</SliderProvider>
+	);
+}
--- ui/src/components/ProductDetails/ProductImages.tsx
+++ ui/src/components/ProductDetails/ProductImages.tsx
@@ -1,22 +1,32 @@
-import { For } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
 
 import type ProductInterface from "../../interfaces/product";
 import { getCompressedImageUrl, getImageUrl } from "../../utils/images";
+import Carousel from "../Carousel";
 
 import styles from "./productDetails.module.sass";
 
 export default function ProductImages(props: { product?: ProductInterface }) {
+	const [showCarousel, setShowCarousel] = createSignal<boolean | number>(false);
+
 	return (
 		<div class={styles.images}>
+			<Show when={showCarousel() !== false}>
+				<Carousel
+					showCarousel={showCarousel}
+					setShowCarousel={setShowCarousel}
+					images={props.product?.images}
+				/>
+			</Show>
 			<For each={props.product?.images}>
-				{(image) => (
-					<a href={getImageUrl(image.url)} target="_blank" rel="noreferrer">
-						<img
-							class={styles.image}
-							src={getCompressedImageUrl(getImageUrl(image.url))}
-							alt={image.description}
-						/>
-					</a>
+				{(image, index) => (
+					<img
+						class={styles.image}
+						src={getCompressedImageUrl(getImageUrl(image.url))}
+						alt={image.description}
+						onClick={() => setShowCarousel(index())}
+						onKeyPress={() => setShowCarousel(index())}
+					/>
 				)}
 			</For>
 		</div>
