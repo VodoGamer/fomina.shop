95557d9e83fe10172bd9ffa012383f3c90bfff1e
VodoGamer <vodogamer@yandex.ru>
2024-09-08 18:17:56 +0300

    ✨ ui: update footer

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -1,25 +1,42 @@
-import type { Component } from "solid-js";
+import { type Component, type JSX, children, splitProps } from "solid-js";
 
 const Footer: Component = () => {
 	return (
 		<footer class="flex flex-wrap gap-x-6 gap-y-2 mt-6">
-			<FooterLink text="instagram" link="https://instagram.com/fominakids" />
-			<FooterLink text="telegram" link="https://t.me/fominakids" />
-			<FooterLink text="условия заказа" link="/order_terms" />
-			<FooterLink text="условия возврата" link="/return_terms" />
+			<FooterLink href="https://instagram.com/fominakids" target="_blank">
+				instagram
+			</FooterLink>
+			<FooterLink href="https://t.me/fominakids" target="_blank">
+				telegram
+			</FooterLink>
+			<FooterLink
+				href="https://www.ozon.ru/brand/fominakids-100260931/"
+				target="_blank"
+			>
+				ozon
+			</FooterLink>
+			<FooterLink href="/order_terms">условия заказа</FooterLink>
+			<FooterLink href="/return_terms">условия возврата</FooterLink>
 		</footer>
 	);
 };
 
 export default Footer;
 
-function FooterLink(props: { text: string; link: string }) {
+function FooterLink(
+	props: {
+		children: JSX.Element;
+	} & JSX.AnchorHTMLAttributes<HTMLAnchorElement>,
+) {
+	const [local, others] = splitProps(props, ["children"]);
+	const c = children(() => local.children);
+
 	return (
 		<a
 			class="text-gray-500 duration-300 ease-out hover:text-gray-900"
-			href={props.link}
+			{...others}
 		>
-			{props.text}
+			{c()}
 		</a>
 	);
 }
