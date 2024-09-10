f10d8a7733196fb29d165fcebb0b02bb7f186b50
VodoGamer <vodogamer@yandex.ru>
2024-09-10 22:44:46 +0300

    ✨  feat: add `HeaderLoader`

--- ui/src/components/Header/index.tsx
+++ ui/src/components/Header/index.tsx
@@ -1,13 +1,19 @@
-import type { Component } from "solid-js";
+import { useIsRouting } from "@solidjs/router";
+import { type Component, Show } from "solid-js";
 
 import Menu from "./Menu";
 
+import { HeaderLoader } from "../HeaderLoader";
 import styles from "./assets/header.module.css";
 import logo from "./assets/logo.svg";
 
 const Header: Component = () => {
+	const isRouting = useIsRouting();
 	return (
 		<header class={styles.header}>
+			<Show when={isRouting()}>
+				<HeaderLoader />
+			</Show>
 			<div class={styles.header__logo}>
 				<a href="/">
 					<img class={styles.header__image} src={logo} alt="" />
---
+++ ui/src/components/HeaderLoader.tsx
@@ -0,0 +1,11 @@
+export function HeaderLoader() {
+	return (
+		<>
+			{
+				<div class="fixed top-0 left-0 w-full h-1 bg-gray-200">
+					<div class="h-full bg-blue-500 animate-pulse w-full" />
+				</div>
+			}
+		</>
+	);
+}
